4295ba42dc6003eab89eb760794d07a7a5bae1b2
Christopher Hoang <christopherhoang707@gmail.com>
2023-02-22 14:08:51 -0800

    Updated for assignment 4

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import folium
 
 app = Flask(__name__)
 
@@ -9,6 +10,12 @@ def hello_world():
 @app.route("/about-us")
 def about():
 	return render_template('index.html')
+
+#Uses Folium to get a map location of Cal Poly Pomona
+@app.route("/map")
+def map():
+    m = folium.Map(location=[34.05577715605838, -117.81930591750536], zoom_start=18)
+    return m.get_root().render()
 
 @app.route("/CS4800")
 def CS4800():

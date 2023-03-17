1f693bf217a223c2b816ebdbec29b2e17e56ff59
Nick Hortua <nhortua@cpp.edu>
2023-03-16 22:10:56 -0700

    updated with test function for water stations (Assignment 6)

--- test/test_map.py
+++ test/test_map.py
@@ -11,3 +11,9 @@ def test_bikeRack_location(app, client):
     assert res.status_code == 200
     data = res.text
     assert data == '["College of Science"]\n', "Bike rack list check failed"
+
+def test_waterStation_location(app, client):
+    res = client.get('/getlocations/water_station')
+    assert res.status_code == 200
+    data = res.text
+    assert data == '["College of Science"]\n', "Test failed on obtaining water station results"
